d33eae0412ccb620c91bdb15ba18175eb395c96a
Caleb Figgers <cdfiggers@gmail.com>
2022-02-05 16:43:32 -0600

    Basic ninja fighting

--- src/ninja_village/ninja.clj
+++ src/ninja_village/ninja.clj
@@ -16,9 +16,32 @@
                                          :ninja/tools]))
 
 (s/def :ninja/rank #{:genin :chunin :jonin :kage})
+(s/def :ninja/practice (s/and int? #(<= % 100) #(>= % 0)))
 (s/def :ninja/ninja (s/keys :req [:core/name
                                   :ninja/rank
-                                  :ninja/ninja-stats]))
+                                  :ninja/ninja-stats
+                                  :ninja/practice]))
 (s/def :ninja/ninjas (s/* :ninja/ninja))
+
+(defn randomstats []
+  (shuffle [:ninja/speed :ninja/jutsu :ninja/health 
+                            :ninja/tools :ninja/stealth]))
+
+(defn generate-skills [level & [academy-quality]]
+  (let [skill-base ({:genin 1 :chunin 2 :jonin 3 :kage 5} level)]
+    (into (sorted-map)
+          (zipmap (randomstats)
+                  (take 5 (concat (vector (min (+ 2 skill-base) 6))
+                                  (repeat (or academy-quality 0) (inc skill-base))
+                                  (repeat skill-base)))))))
+
+
+(defn generate-ninja [& [rank academy-quality]]
+  {:core/name "Ninja Trainee"
+   :ninja/rank (or rank :genin)
+   :ninja/ninja-stats (generate-skills
+                       (or rank :genin)
+                       (or academy-quality 0))
+   :ninja/practice 0})
 
 
